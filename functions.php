67948a82d1f32b3db6923a6ed0a957ac5cf52564
satanas <wil.alejandro@gmail.com>
2011-11-07 23:47:18 -0300

    Changed version of jQuery from google api

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ add_action('init', 'my_init_method');
 function my_init_method() {
     if (!is_admin()) {
         wp_deregister_script( 'jquery' );
-        wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js');
+        wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js');
         wp_enqueue_script( 'jquery' );
     }
 }

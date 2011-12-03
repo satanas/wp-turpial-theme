8ad0b4f427c9a383ed3d6e024335700bb11ffa95
satanas <wil.alejandro@gmail.com>
2011-12-03 17:09:16 -0300

    Modified header for maintenance mode

--- 503.php
+++ 503.php
@@ -14,7 +14,7 @@
   <div id="container">
     <img id="logo" src="http://turpial.org.ve/wp-content/themes/turpial-2.0/images/turpial-logo-maintenance.png" alt="Turpial logo" width="165"/>
     <img id="pajarito-morido" src="http://turpial.org.ve/wp-content/themes/turpial-2.0/images/turpial-wet-floor.png" alt="Turpial est&aacute; morido" width="190"/>
-    <h1>Out of service</h1>
+    <h1>Maintenance</h1>
     <p>We're cleaning da house. Please come back in a while</p>
       <p id="links"><a href="http://dev.turpial.org.ve/projects/turpial/issues/">Report bug</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://twitter.com/turpialve">Turpial in twitter</a></p>
   </div>

12cb2060307eb8ea29e4a29d53c9ea72ba0eb7d3
Wil Alvarez <wil.alejandro@gmail.com>
2012-02-03 15:51:06 -0300

    Added adsense to blog pages

--- page.php
+++ page.php
@@ -5,6 +5,21 @@
         <div id="page-head">
               <h1><?php the_title(); ?></h1>
         </div>
+        <div id="adsense_container">
+          <div id="turpial_adsense">
+            <script type="text/javascript"><!--
+            google_ad_client = "ca-pub-9865703181407066";
+            /* Turpial Footer */
+            google_ad_slot = "1694063806";
+            google_ad_width = 728;
+            google_ad_height = 90;
+            //-->
+            </script>
+            <script type="text/javascript"
+            src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+            </script>
+          </div>
+        </div>
         <div id="page">
             <?php the_content(); ?>
         </div>

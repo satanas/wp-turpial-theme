a0dbf0575971df446819ccd4c7c27cd9fe299c97
Wil Alvarez <wil.alejandro@gmail.com>
2012-02-03 15:46:33 -0300

    Added turpial adsense into another div

--- frontpage.php
+++ frontpage.php
@@ -42,18 +42,20 @@
         <span class="more"><a href="features/">learn more...</a></span>
         <div class="clearbox"></div>
       </div>
-      <div id="turpial_adsense">
-        <script type="text/javascript"><!--
-        google_ad_client = "ca-pub-9865703181407066";
-        /* Turpial Footer */
-        google_ad_slot = "1694063806";
-        google_ad_width = 728;
-        google_ad_height = 90;
-        //-->
-        </script>
-        <script type="text/javascript"
-        src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-        </script>
+      <div id="adsense_container">
+          <div id="turpial_adsense">
+            <script type="text/javascript"><!--
+            google_ad_client = "ca-pub-9865703181407066";
+            /* Turpial Footer */
+            google_ad_slot = "1694063806";
+            google_ad_width = 728;
+            google_ad_height = 90;
+            //-->
+            </script>
+            <script type="text/javascript"
+            src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+            </script>
+          </div>
       </div>
       <div id="page">
         <div id="news">

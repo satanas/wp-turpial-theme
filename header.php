874e495010d89c86ea1e5c8ef31d31992bd9a585
satanas <wil.alejandro@gmail.com>
2012-10-03 00:55:30 -0300

    Changed paypal donation button

--- header.php
+++ header.php
@@ -63,7 +63,7 @@
 		<a href="http://flattr.com/thing/452623/Turpial-Website" target="_blank">
 		<img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a>
 	</noscript>
-	<a id="paypal-donate" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=XUNXXJURA7FLW"><img src="https://www.paypal.com/en_US/i/btn/x-click-but04.gif" alt="" /></a>
+	<a id="paypal-donate" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3QDYV3JLXPQ7U"><img src="https://www.paypal.com/en_US/i/btn/x-click-but04.gif" alt="" /></a>
         <a id="rss-icon" href="http://turpial.org.ve/feed/" title="Feed RSS"></a>
         <a id="twitter-icon" href="http://twitter.com/turpialve" title="@turpialve"></a>
         <div class="clearbox"></div>

1ac5e6d83d17fac8aadd1b2a34817b8c6e983d84
Wil Alvarez <wil.alvarez@groupon.com>
2014-01-14 19:30:42 -0300

    Added forgotten file

---
+++ footer_ads.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Footer Ads
+*/
+?>
+
+<div id="adsense_container">
+    <div id="turpial_ads">
+        <script type="text/javascript"><!--
+        google_ad_client = "ca-pub-9865703181407066";
+        /* Turpial Footer */
+        google_ad_slot = "1694063806";
+        google_ad_width = 728;
+        google_ad_height = 90;
+        //-->
+        </script>
+        <script type="text/javascript"
+        src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+        </script>
+    </div>
+</div>

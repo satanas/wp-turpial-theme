2d781825b3a25d3654d94aa0d5b657a0c45b9140
satanas <wil.alejandro@gmail.com>
2014-01-15 00:04:00 -0300

    Removed flattr and paypal. Adjusted size

--- header.php
+++ header.php
@@ -7,13 +7,13 @@
             <li><a class="twitter menu-icon" href="http://twitter.com/turpialve" title="@turpialve"></a></li>
             <li><a class="google-plus menu-icon" href="" title="TurpialVe G+"></a></li>
             <li><a class="rss menu-icon" href="http://turpial.org.ve/feed/" title="Feed RSS"></a></li>
-            <li><a class="FlattrButton" style="display:none;" href="http://turpial.org.ve"></a>
+            <!-- li><a class="FlattrButton" style="display:none;" href="http://turpial.org.ve"></a>
                 <noscript>
                   <a href="http://flattr.com/thing/452623/Turpial-Website" target="_blank">
                   <img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a>
                 </noscript>
             </li>
-            <li><a id="paypal-donate" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3QDYV3JLXPQ7U"><img src="https://www.paypal.com/en_US/i/btn/x-click-but04.gif" alt="" /></a></li>
+            <li><a id="paypal-donate" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3QDYV3JLXPQ7U"><img src="https://www.paypal.com/en_US/i/btn/x-click-but04.gif" alt="" /></a></li -->
         </ul>
     </div>
 </div>

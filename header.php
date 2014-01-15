08e0617c4ed2c2de9ca288288e293f853551a8a9
Wil Alvarez <wil.alvarez@groupon.com>
2014-01-14 23:38:44 -0300

    Added paypal and flattr buttons

--- header.php
+++ header.php
@@ -6,6 +6,14 @@
             <li><a class="menu-item" href="http://wiki.turpial.org.ve">Wiki</a></li>
             <li><a class="twitter menu-icon" href="http://twitter.com/turpialve" title="@turpialve"></a></li>
             <li><a class="google-plus menu-icon" href="" title="TurpialVe G+"></a></li>
+            <li><a class="rss menu-icon" href="http://turpial.org.ve/feed/" title="Feed RSS"></a></li>
+            <li><a class="FlattrButton" style="display:none;" href="http://turpial.org.ve"></a>
+                <noscript>
+                  <a href="http://flattr.com/thing/452623/Turpial-Website" target="_blank">
+                  <img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a>
+                </noscript>
+            </li>
+            <li><a id="paypal-donate" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3QDYV3JLXPQ7U"><img src="https://www.paypal.com/en_US/i/btn/x-click-but04.gif" alt="" /></a></li>
         </ul>
     </div>
 </div>
@@ -13,13 +21,7 @@
 <div id="header">
     <a id="logo" href="<?php bloginfo('url'); ?>"></a>
     <div id="contact-icons">
-        <a class="FlattrButton" style="display:none;" href="http://turpial.org.ve"></a>
-        <noscript>
-          <a href="http://flattr.com/thing/452623/Turpial-Website" target="_blank">
-          <img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a>
-        </noscript>
-        <a id="paypal-donate" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3QDYV3JLXPQ7U"><img src="https://www.paypal.com/en_US/i/btn/x-click-but04.gif" alt="" /></a>
-        <a id="rss-icon" href="http://turpial.org.ve/feed/" title="Feed RSS"></a>
+        
         <div class="clearbox"></div>
     </div>
 </div>

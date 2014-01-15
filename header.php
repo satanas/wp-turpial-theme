3d55938232e4748ac6500ff575be4b4a0b91f48f
Wil Alvarez <wil.alvarez@groupon.com>
2014-01-14 23:33:34 -0300

    Fixing style of menu items

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <div id="navbar">
     <div id="menu">
         <ul>
-            <li><a href="<?php bloginfo('url'); ?>">Home</a></li>
+            <li><a class="menu-item" href="<?php bloginfo('url'); ?>">Home</a></li>
             <?php wp_list_pages('title_li=&depth=1'); ?>
-            <li><a href="http://wiki.turpial.org.ve">Wiki</a></li>
+            <li><a class="menu-item" href="http://wiki.turpial.org.ve">Wiki</a></li>
             <li><a class="twitter menu-icon" href="http://twitter.com/turpialve" title="@turpialve"></a></li>
             <li><a class="google-plus menu-icon" href="" title="TurpialVe G+"></a></li>
         </ul>

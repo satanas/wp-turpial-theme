7d74883066f3bad93b04c0fcbeb77c1b3e0bd3dd
satanas <wil.alejandro@gmail.com>
2014-01-16 15:07:02 -0300

    Testing bigger thumbnails

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 add_theme_support( 'post-thumbnails' );
-set_post_thumbnail_size(210, 158, true);
+set_post_thumbnail_size(234, 176);
 add_image_size('single-post-thumbnail', 450, 338);
 
 if ( function_exists('register_sidebar') )

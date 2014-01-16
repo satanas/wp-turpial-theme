fa126ce431f5fc64c221ef7b63ad843ef7a3f4d2
satanas <wil.alejandro@gmail.com>
2014-01-16 18:32:58 -0300

    Testing with ul

--- index.php
+++ index.php
@@ -10,12 +10,12 @@
     <?php get_header(); ?>
     <div id="container">
         <div id="page">
-            <div id="previews">
+            <ul id="previews">
                 <?php $my_query = new WP_Query('showposts=8'); ?>
                 <?php if($my_query->have_posts()) : ?>
                     <?php $counter = 0; ?>
                     <?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
-                    <div class="post-preview">
+                    <li class="post-preview">
                         <?php the_post_thumbnail('single-post-thumbnail'); ?>
                         <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 
@@ -24,16 +24,17 @@
                             <span class="author"><?php _e('By '); the_author_posts_link(); edit_post_link('Edit', ' &#124; ', ''); ?></span>
                         </span>
                         <?php echo get_the_excerpt(); ?> <a href="<?php the_permalink(); ?>">Read full article</a>
-                    </div>
+                    </li>
                     <?php $counter++; ?>
                     <?php if($counter > 1) : ?>
                         <?php $counter = 0; ?>
-                        <div class="clearbox"></div>
+                        <!-- div class="clearbox"></div -->
                     <?php endif; ?>
                     <?php endwhile; ?>
                     <div id="navigation">
                         <p><?php posts_nav_link(); ?></p>
                     </div>
+            </ul>
                 <?php else : ?>
                 <div class="post">
                     <div class="post-head">
@@ -41,8 +42,8 @@
                     </div>
                 </div>
                 <?php endif; ?>
-                <div class="clearbox"></div>
-            </div>
+                <!-- div class="clearbox"></div -->
+            <!-- /div-->
         </div>
     </div>
 

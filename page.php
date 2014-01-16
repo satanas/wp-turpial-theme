f28b996e6dd7990582e955a0b2dd3d695a6a98d7
satanas <wil.alejandro@gmail.com>
2014-01-16 20:36:38 -0300

    Fixed activate on download button

--- page.php
+++ page.php
@@ -19,7 +19,7 @@
                         <div class="turpial_adsense_footer">
                             <script type="text/javascript"><!--
                             google_ad_client = "ca-pub-9865703181407066";
-                            /* Turpial Footer */
+                            /* Turpial Header */
                             google_ad_slot = "6621834408";
                             google_ad_width = 728;
                             google_ad_height = 90;

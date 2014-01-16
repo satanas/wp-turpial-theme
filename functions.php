0d6d77d73c74d4dafcac8d82419d00dfd6de0107
satanas <wil.alejandro@gmail.com>
2014-01-16 15:14:05 -0300

    Updated style

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 add_theme_support( 'post-thumbnails' );
-set_post_thumbnail_size(234, 176);
+set_post_thumbnail_size(220, 165, true);
 add_image_size('single-post-thumbnail', 450, 338);
 
 if ( function_exists('register_sidebar') )

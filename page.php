bb0e64051fd02e63f13bacf371c720ea8baa1358
satanas <wil.alejandro@gmail.com>
2014-01-16 19:54:06 -0300

    First layout change in page

--- page.php
+++ page.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <?php get_template_part('head'); ?>
+</head>
+
+<body>
+    <?php get_header(); ?>
+    <div id="container">
+        <div id="page">
+            <?php the_content(); ?>
+        </div>
+    </div>
+
+    <div class="push"></div>
+    <?php get_footer(); ?>
+</body>
+</html>
+
+
 <?php get_header(); ?>
     <div id="content">
       <?php if(have_posts()) : ?>

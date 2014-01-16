bb3770ec9ece0a4e0f4a60bc54e3984f7d206d3e
satanas <wil.alejandro@gmail.com>
2014-01-16 13:34:24 -0300

    Added thumbnail sizes

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 add_theme_support( 'post-thumbnails' );
-set_post_thumbnail_size(320, 240, true);
+set_post_thumbnail_size(210, 158, true);
+add_image_size('single-post-thumbnail', 450, 338);
 
 if ( function_exists('register_sidebar') )
     register_sidebar();

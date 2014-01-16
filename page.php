dcc547c2c857e274b05d81685b79d060c640515d
satanas <wil.alejandro@gmail.com>
2014-01-16 20:32:22 -0300

    Modified adsense for pages and minor style fixes

--- page.php
+++ page.php
@@ -16,21 +16,20 @@
                     <h1><?php the_title(); ?></h1>
 
                     <div id="adsense_container">
-                      <div id="turpial_adsense">
-                        <script type="text/javascript"><!--
-                        google_ad_client = "ca-pub-9865703181407066";
-                        /* Turpial Footer */
-                        google_ad_slot = "1694063806";
-                        google_ad_width = 728;
-                        google_ad_height = 90;
-                        //-->
-                        </script>
-                        <script type="text/javascript"
-                        src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-                        </script>
-                      </div>
+                        <div class="turpial_adsense_footer">
+                            <script type="text/javascript"><!--
+                            google_ad_client = "ca-pub-9865703181407066";
+                            /* Turpial Footer */
+                            google_ad_slot = "6621834408";
+                            google_ad_width = 728;
+                            google_ad_height = 90;
+                            //-->
+                            </script>
+                            <script type="text/javascript"
+                            src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+                            </script>
+                        </div>
                     </div>
-
 
                     <div id="content"><?php the_content(); ?></div>
 

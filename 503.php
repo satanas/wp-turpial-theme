9e6904607ef3f007f505f4617ddd85eef04ceae0
satanas <wil.alejandro@gmail.com>
2014-01-16 23:08:40 -0300

    Removed images. Working with maintenance page

--- 503.php
+++ 503.php
@@ -1,22 +1,30 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
+    <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+    <meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
+    <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+    <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
+    <link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico" />
 
-<title>Turpial - Out of service</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<link rel="stylesheet" href="http://turpial.org.ve/wp-content/themes/turpial-2.0/maintenance.css" type="text/css" media="screen" />
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400' rel='stylesheet' type='text/css'>
+    <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php get_template_directory(); ?>/maintenance.css" type="text/css" media="screen" />
 </head>
 
 <body>
-  <div id="container">
-    <img id="logo" src="http://turpial.org.ve/wp-content/themes/turpial-2.0/images/turpial-logo-maintenance.png" alt="Turpial logo" width="165"/>
-    <img id="pajarito-morido" src="http://turpial.org.ve/wp-content/themes/turpial-2.0/images/turpial-wet-floor.png" alt="Wet Floor" width="190"/>
-    <h1></h1>
-    <p>We're cleaning da house. Please come back in a while</p>
-      <p id="links"><a href="http://dev.turpial.org.ve/projects/turpial/issues/">Report bug</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://twitter.com/turpialve">Turpial in twitter</a></p>
-  </div>
+    <div id="container">
+        <div id="page">
+        </div>
+    </div>
+
+    <div class="push"></div>
+    <?php get_footer(); ?>
 </body>
 </html>

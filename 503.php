aec1f788b903b9baa869119cd720b02600e6ecc1
satanas <wil.alejandro@gmail.com>
2014-01-16 23:16:38 -0300

    Fixing 503

--- 503.php
+++ 503.php
@@ -3,18 +3,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-    <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
-    <meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-    <meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
-    <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
-    <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
-    <link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
-    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico" />
+    <title>Turpial - Under maintenance</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link rel="shortcut icon" href="http://turpial.org.ve/wp-content/themes/turpial/images/favicon.ico" />
 
     <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/maintenance.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="http://turpial.org.ve/wp-content/themes/turpial/maintenance.css" type="text/css" media="screen" />
 </head>
 
 <body>
@@ -22,8 +17,5 @@
         <div id="page">
         </div>
     </div>
-
-    <div class="push"></div>
-    <?php get_footer(); ?>
 </body>
 </html>

d623101fac853ab215704092d9d0a8008e3c6ea4
satanas <wil.alejandro@gmail.com>
2014-01-16 22:32:08 -0300

    Added new template for donate

--- head.php
+++ head.php
@@ -36,7 +36,7 @@ _gaq.push(['_trackPageview']);
         var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
         s.type = 'text/javascript';
         s.async = true;
-        s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto&button=compact';
+        s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
         t.parentNode.insertBefore(s, t);
     })();
 /* ]]> */
